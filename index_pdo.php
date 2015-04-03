959d8c9960fe211420569271a368b9efd511fd96
darkontes1 <darkontes@hotmail.fr>
2015-04-03 10:16:58 +0200

    on sort quand du twitter

--- index_pdo.php
+++ index_pdo.php
@@ -1,6 +1,24 @@
 <?php
-
-    include('json.php');
+//session_start();
+//session_destroy();
+session_start();
+    $meow = 'Meow_kitty_cat';
+    if(empty($_SESSION)){  //Préférer utiliser empty plutôt que isset pour les session car un session_start déclare une fonction directement
+        $_SESSION['nb'] = 0;
+        $_SESSION['connect'] = FALSE;
+        $_SESSION['login'] = $meow;
+        $_SESSION['id'] = -1;
+        $_SESSION['idTwit'] = '';
+    }
+    try{
+    //Syntaxe init PDO => $host;$BDD,$name,$mdp
+        $db = new PDO('mysql:host=localhost;dbname=twitr','root','');
+    } catch(PDOException $ex){
+       echo '<br/>';
+       echo 'echec lors de la connexion a MySQL : ('.$ex->getCode().')';
+       echo $ex->getMessage();
+       exit();
+    }
     //var_dump($_SESSION);
     //bouton prec est appuyé
     if(isset($_POST['prec'])){
